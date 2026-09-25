45862fae16210acfcdce41c90d4ba416d52c6510
Aldo Hassiel Acosta Espinoza <aldohassielacosta@gmail.com>
2026-09-24 20:37:06 -0700

    feat(horas): importar texto crudo a esquema de horas
    Se añade una ruta para importar texto crudo y una función que lo
    convierte en registros con RPE y TOTAL.

--- modulos/horas/rutas.py
+++ modulos/horas/rutas.py
@@ -1,7 +1,12 @@
-from fastapi import APIRouter, UploadFile
+from typing import Annotated
+
+from fastapi import APIRouter, Form, UploadFile
 
 from dependencias import SessionDep
-from modulos.horas.servicio import acumular_horas_extras
+from modulos.horas.servicio import (
+    acumular_horas_extras,
+    transformar_texto_a_esquema_horas,
+)
 from nucleo.respuestas import RespuestaAPI
 from nucleo.utils import leer_archivo, validar_datos
 
@@ -23,3 +28,10 @@ async def importar_horas_extras(archivo: UploadFile, session: SessionDep):
     conjunto = acumular_horas_extras(registros)
 
     return conjunto
+
+
+@ruta.post("/importar-texto")
+async def importar_texto_crudo(texto: Annotated[str, Form(...)]):
+    texto_limpio = transformar_texto_a_esquema_horas(texto)
+
+    return texto_limpio
--- modulos/horas/servicio.py
+++ modulos/horas/servicio.py
@@ -1,5 +1,7 @@
 from datetime import time, timedelta
 
+from sqlmodel import text
+
 from nucleo.utils import convertir_a_time
 
 
@@ -34,3 +36,29 @@ def acumular_horas_extras(datos: list[dict[str, time]]):
         resultado[rpe] = f"{horas:02d}:{minutos:02d}:{segundos:02d}"
 
     return resultado
+
+
+def transformar_texto_a_esquema_horas(texto_crudo: str):
+    texto_lista = texto_crudo.split("\t")
+
+    datos_transformados: list[dict[str, str]] = []
+
+    INICIO = 17
+    SALTO_A_HORA = 6
+    SALTO_DE_HORA_A_RPE = 11
+
+    indice = INICIO
+
+    while indice < len(texto_lista) - 1:
+        elemento_rpe = texto_lista[indice]
+        indice_rpe = 2 if "SIN AUTORIZACION" in elemento_rpe else 1
+        rpe = elemento_rpe.split(" ")[indice_rpe]
+
+        indice += SALTO_A_HORA
+
+        hora = texto_lista[indice]
+        indice += SALTO_DE_HORA_A_RPE
+
+        datos_transformados.append({"RPE": rpe, "TOTAL": hora})
+
+    return datos_transformados
